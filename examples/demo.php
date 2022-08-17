50404245b4d72c1da06c67858902718d20370b7f
Andrey Pakhomov <andrey.pahomov@gmail.com>
2022-08-18 01:50:15 +0300

    Update demo.php

--- examples/demo.php
+++ examples/demo.php
@@ -4,24 +4,25 @@ declare(strict_types=1);
 /**
  * DEMO: Using Logger with DI
  */
+
 use VPA\DI\Container;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 // We want use for logs JS Developer Console if we run the script in browser and CLI Console - if run in CLI mode
-$classLogger = php_sapi_name()=='cli' ? 'VPA\Logger\ConsoleLogger' : 'VPA\Logger\JSConsoleLogger';
+$classLogger = php_sapi_name() == 'cli' ? 'VPA\Logger\ConsoleLogger' : 'VPA\Logger\JSConsoleLogger';
 
 $di = new Container();
 $di->registerContainers([
-    'VPA\Logger'=>$classLogger,
+    'VPA\Logger' => $classLogger,
 ]);
 
 $logger = $di->get('VPA\Logger');
-$logger->emergency("emergency message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->alert("alert message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->critical("critical message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->error("error message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->warning("warning message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->notice("notice message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->info("info message {date}",['date'=>date('d.m.Y H:i:s')]);
-$logger->debug("debug message {date}",['date'=>date('d.m.Y H:i:s')]);
+$logger->emergency("emergency message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->alert("alert message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->critical("critical message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->error("error message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->warning("warning message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->notice("notice message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->info("info message {date}", ['date' => date('d.m.Y H:i:s')]);
+$logger->debug("debug message {date}", ['date' => date('d.m.Y H:i:s')]);
